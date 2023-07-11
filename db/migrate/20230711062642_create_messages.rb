516c7cf81197fe01c1a29acd44d8eaf39cae1360
AmanWorku <amexworku2014@gmail.com>
2023-07-11 09:27:12 +0300

    Modify migration

--- db/migrate/20230711062642_create_messages.rb
+++ db/migrate/20230711062642_create_messages.rb
@@ -1,8 +1,7 @@
-class CreateMessages < ActiveRecord::Migration[7.0]
+class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
       t.string :greeting
-
       t.timestamps
     end
   end
