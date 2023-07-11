d29bdb6a626cdd1b6a8ebe97a91c4ddd3ebdc86f
AmanWorku <amexworku2014@gmail.com>
2023-07-11 09:34:31 +0300

    Update controller to select random greeting

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,6 @@
 class GreetingsController < ApplicationController
   def random_greeting
+    @greeting = Message.order('RANDOM()').first&.greeting
+    render json: { greeting: @greeting }
   end
 end
