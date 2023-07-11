324c060b2d72b31f2a439980dba0b4af20e3175d
AmanWorku <amexworku2014@gmail.com>
2023-07-11 10:55:40 +0300

    Refactor js part

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,12 @@
-import { Application } from "@hotwired/stimulus";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import App from "./components/App";
 
-const application = Application.start();
-
-// Configure Stimulus development experience
-application.debug = false;
-window.Stimulus = application;
-
-export { application };
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
